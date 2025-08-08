0d70efa74d7b7d41427db64958b432a689ff2052
Victor Moraes <victormoraespr@gmail.com>
2025-08-08 18:14:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import pycountry
 
 # Configuração da página
 # Definie o título da página, o ícone e o layout para ocupar toda a largura
